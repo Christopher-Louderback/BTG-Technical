c2149df06a456caa8873e05257d8c5aa5baca2ff
Jake <85717369+Christopher-Louderback@users.noreply.github.com>
2026-07-21 01:28:49 -0400

    Add project files.

---
+++ BTG Technical/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+/*
+ * class to open + read
+ * prompt user for file name, generate output file
+ * class to extract
+ * call all in main
+ * class to validate and calculate
+ * class to produce json output*/
