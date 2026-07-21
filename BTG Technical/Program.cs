4b84cb5078373934c1252a762c34be9119c80722
Jake <85717369+Christopher-Louderback@users.noreply.github.com>
2026-07-21 01:43:06 -0400

    Initial structuring/thoughts

--- BTG Technical/Program.cs
+++ BTG Technical/Program.cs
@@ -1,5 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//take args 0 and 1, 0 = csv, 1 = where writing to
+//assign to variable and pass to reader (di)
+//reader/extractor(?) assigns results to ds to pass to processor
+//pass product to writer
+
+
+
 /*
  * class to open + read
  * prompt user for file name, generate output file
---
+++ BTG Technical/Writer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTG_Technical
+{
+    internal class Writer
+    {
+    }
+    //write output to file
+    //Produce JSON output containing the transformed records.
+}
