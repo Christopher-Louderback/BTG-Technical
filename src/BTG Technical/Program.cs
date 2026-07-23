70cfbd2ec4e34c993c5c0fe83c528dfee8da4bc9
Jake <85717369+Christopher-Louderback@users.noreply.github.com>
2026-07-22 21:08:10 -0400

    Error handling for malformed input

--- src/BTG Technical/Program.cs
+++ src/BTG Technical/Program.cs
@@ -1,26 +1,37 @@
 ﻿using BTG_Technical;
-
+ 
 if (args.Length == 0)
 {
     Console.Error.WriteLine("Error: No input file specified.");
     return;
 }
-
+ 
 string? inputFile = args[0];
-
+ 
 if (!File.Exists(inputFile))
 {
     Console.Error.WriteLine($"Error: File {inputFile} not found.");
     return;
 }
-
+ 
 string outputFile = Path.ChangeExtension(inputFile, ".json");
-
-Console.WriteLine("Reading file and extracting data.");
-List<Record> data = Reader.ReadFile(inputFile);
-
-Console.WriteLine("Processing data.");
-List<Record> processedData = RecordProcessor.Process(data);
-
-Console.WriteLine("Creating JSON.");
-Writer.Write(outputFile, processedData);
+ 
+try
+{
+    Console.WriteLine("Reading file and extracting data.");
+    List<Record> data = Reader.ReadFile(inputFile);
+ 
+    Console.WriteLine("Processing data.");
+    List<Record> processedData = RecordProcessor.Process(data);
+ 
+    Console.WriteLine("Creating JSON.");
+    Writer.Write(outputFile, processedData);
+}
+catch (InvalidOperationException ex)
+{
+    Console.Error.WriteLine($"Error: {ex.Message}");
+    if (ex.InnerException != null)
+    {
+        Console.Error.WriteLine($"Details: {ex.InnerException.Message}");
+    }
+}
